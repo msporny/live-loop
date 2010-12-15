af82e52d3d28be0ce428abdc620b1f481c2d70c2
Manu Sporny <msporny@digitalbazaar.com>
2010-12-15 03:40:40 -0500

    Added a delay before requesting that the document is parsed for triples.

--- live-loop.js
+++ live-loop.js
@@ -7,6 +7,9 @@
    window.liveloop = window.liveloop || {};
    var liveloop = window.liveloop;
 
+   liveloop.counter = 1;
+   liveloop.counterTimeout = null;
+
    liveloop.generateCodePrologue = function()
    {
       return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML+RDFa 1.0//EN\" \"http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd\">\n<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:dc=\"http://purl.org/dc/terms/\">\n <head>\n  <title>Test Snippet</title>\n </head>\n <body>\n";
@@ -35,10 +38,38 @@
    {
       liveloop.setupCodePrologue();
       liveloop.setupCodeEpilogue();
-      $("#code-body").keyup(liveloop.updateTriples);
+      $("#code-body").keyup(liveloop.checkCounter);
    };
+
+   liveloop.checkCounter = function(event)
+   {
+       if(event)
+       {
+          liveloop.counter = 1;
+       }
+       else
+       {
+          liveloop.counter--;
+       }
+       
+       if(liveloop.counter <= 0)
+       {
+          $("#loading").hide();
+          liveloop.updateTriples();
+       }
+       else
+       {
+          $("#loading").show();
+          if(liveloop.counterTimeout)
+          {
+             clearTimeout(liveloop.counterTimeout);
+          }
+          liveloop.counterTimeout = setTimeout(liveloop.checkCounter, 1000);
+       }
+   };
+
    
-   liveloop.updateTriples = function(event)
+   liveloop.updateTriples = function()
    {
       var rdfaDocument = liveloop.generateCodePrologue() + 
          $("#code-body").val() + liveloop.generateCodeEpilogue();
